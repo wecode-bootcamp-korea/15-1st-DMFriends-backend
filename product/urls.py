aca893110e5668d4a7dac96b1a802842fb138327
lyla0427 <hillary0427@gmail.com>
2020-12-20 20:31:57 +0900

    Fix: Convention 맞게 수정

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
+from django.urls   import path
 from product.views import ProductView, ReviewView, CategoryView
 
 urlpatterns = [path('product/<int:id>', ProductView.as_view()),
-path('product/category', CategoryView.as_view()),
-path('review', ReviewView.as_view()),
+               path('product/category', CategoryView.as_view()),
+               path('review', ReviewView.as_view()),
 ]
