82a99849926c96bea0bb08aa7bad331c9758455c
hwukjunwoo <hwukjunwoo@gmail.com>
2020-12-21 03:28:24 +0900

    로그인 및 추가 기능 구현

--- user/views.py
+++ user/views.py
@@ -1,13 +1,14 @@
 import json, bcrypt, jwt, re
 import my_settings
 
-
+from django.db.models import Q
+from .utils import token_check
 from django.views import View
 from django.http import JsonResponse, HttpResponse
-from user.models import Member, RecentView
+from user.models import Member, RecentView, BoardLike, CommentLike
+from board.models. import Board
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
-
 
 class SignUpView(View):
     def post(self, request):
@@ -40,4 +41,84 @@ class SignUpView(View):
         except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
 
+class LoginView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+
+        if not 'email' in data or not 'password' in data:
+            return JsonResponse({'message':'CHECK_DATA'}, status=400)
+
+        members = Member.objects.filter(email=data['email'])
+        if not members.exists():
+            return JsonResponse({'message':'INVALITD_USER'}, status=400)
+
+        member_data = members.get()
+
+        if bcrypt.checkpw(data['password'].encode('utf-8'), member_data.password.encode('utf-8')):
+            token = jwt.encode({'member_id':member_data.id}, my_settings.SECRET_KEY, my_settings.ALGORITHM).decode('utf-8')
+            return JsonResponse({'message':'SUCCESS_LOGIN', 'token':token}, status=200)
+
+        return JsonResponse({'message': 'INVALID_PASSWORD'}, status=400)
+
+        def login_decorator(func):
+            def wrapper(self, request, *args, **kwargs):
+            try:
+                auth_token = request.headers.get('Authorization', None)
+                payload = jwt.decode(auth_token, my_settings.SECRET_KEY, my_settings.ALGORITHM)
+                request.user = Member.objects.get(id=payload['member_id'])
+                return func(self, request, *args, **kwargs)
+
+            except User.DoesNotExist:
+                return JsonResponse({"message" : "INVALID_MEMBER"}, status=400)
+            except jwt.exceptions.DecodeError:
+                return JsonResponse({"message" : "INVALID_TOKEN"}, status=400)
+            
+        return wrapper
+
+class BoardLikeView(View):
+    @login_decorator
+    def post(self, request):
+        data = json.loads(request.body)
+
+        if not BoardLike.objects.filter(board_id=request.data['board.id'], member_id=request.user.id).exist():
+            BoardLike.objects.create(
+                member =request.user,
+                board=Board.objects.get(id=data['board_id'])
+            )
+            return JsonResponse({'message': 'ADD'}, status=200)
+        delete_like = BoardLike.objects.get(board_id=data['board_id'], member_id=request.user.id)
+        delete_like.delete()
+
+        return JsonResponse({'message':'DELETE'})
+
+class CommentLikeView(View):
+    @login_decorator
+    def post(self, request):
+        data = json.loads(request.body)
+
+        if not CommentLike.objects.filter(comment_id=request.data['comment.id'], member_id=request.user.id).exist():
+            CommentLike.objects.create(
+                member =request.user,
+                comment=Comment.objects.get(id=data['comment_id'])
+            )
+            return JsonResponse({'message': 'ADD'}, status=200)
+        delete_like = CommentLike.objects.get(comment_id=data['comment_id'], member_id=request.user.id)
+        delete_like.delete()
+
+        return JsonResponse({'message':'DELETE'})
+
+class RecentView(View):
+    @login_decorator
+    def get(self, request):        
+        product_id = request.GET.get('product_id')
+        member_id = request.GET.get('member_id')
+
+        if RecentView.objects.filter(product_id=product_id, member_id=member_id).exist():
+            RecentView.objects.create(
+                member_id=member_id,
+                product_id=product_id  
+            )
+            viewed_at = RecentView.objects.all().order_by('-viewed_at')[0:50]
+            return JsonResponse(['message':'RECENTVIEWED ADD'],status=200)
+        
 
