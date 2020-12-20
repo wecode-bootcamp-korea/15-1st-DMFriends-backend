dc142a65384aadf3b476c0740a65cf58067253e7
Hailee <haileeyu21@gmail.com>
2020-12-21 04:12:22 +0900

    [유하람] '오늘'탭 게시물 목록 조회 - 좋아요 수 가장 높은 댓글 표출 ver.

--- board/views.py
+++ board/views.py
@@ -17,23 +17,35 @@ class IndexView(View):
                 board_id    = i.id
                 # 가장 최근의 댓글 목록[0] 가져오기
                 comments      = []
-                comment_data = Comment.objects.filter(
+                ## 1. 해당 게시물의 댓글 id 추출
+                comment_list = Comment.objects.filter(
                                     board_id = board_id
-                                ).order_by('created_at')
-                # 댓글이 있을 경우 board 리스트에 담을 준비
-                if comment_data:
-                    member_id = comment_data[0].writer_id
-                    comments.append({
-                        'comment_num'   : len(comment_data),
-                        'writer'        : Member.objects.filter(id = comment_data[0].writer_id)[0].nickname,
-                        'content'       : comment_data[0].content
-                    })
+                                )
+                comment_ids = []
+                for j in range(len(comment_list)):
+                    comment_ids.append(comment_list[j].id)
+                ## 2. 각 댓글의 좋아요 수 sum list 중 가장 큰 값 추출 
+                comment_sums = {} 
+                for j in range(len(comment_ids)):
+                    comment_sums[comment_ids[j]] = CommentLike.objects.filter(comment_id =comment_ids[j]).count()
+                # 3. 댓글이 아예 없는 경우 대비하기
+                if comment_ids:
+                    best_comment_id = max(comment_sums, key = comment_sums.get)
+                    comment_data = comment_list.filter(id = best_comment_id)
+                    # 댓글이 있을 경우 board 리스트에 담을 준비
+                    if comment_data:
+                        member_id = comment_data[0].writer_id
+                        comments.append({
+                            'comment_num'   : len(comment_data),
+                            'writer'        : Member.objects.filter(id = comment_data[0].writer_id)[0].nickname,
+                            'content'       : comment_data[0].content
+                        })
                 # board image 준비
                 board_images        = []
                 board_image_data    = BoardImage.objects.filter(board_id = board_id)
-                for j in range(len(board_image_data)):
+                for k in range(len(board_image_data)):
                     board_images.append(
-                        board_image_data[j].image_url
+                        board_image_data[k].image_url
                     )
                 # 게시물 좋아요 수 가져오기
                 board_likes = BoardLike.objects.filter(board_id = board_id)
