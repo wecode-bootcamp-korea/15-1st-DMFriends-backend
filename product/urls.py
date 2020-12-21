64ca0abff627af557bf4e72ffd3e994b80ffb59b
lyla0427 <72369222+lyla0427@users.noreply.github.com>
2020-12-21 10:31:59 +0900

    Modify: 변수명 직관적으로 변경
    product/urls.py에서 본래 id였던 변수명을 product_id로 변경
    
    Co-authored-by: ulfrid <fergith@naver.com>

--- product/urls.py
+++ product/urls.py
@@ -2,6 +2,6 @@ from django.urls   import path
 from product.views import ProductView, ReviewView, CategoryView
 
 urlpatterns = [path('/detail/<int:product_id>', ProductView.as_view()),
-               path('product/category', CategoryView.as_view()),
-               path('product/<int:id>/review', ReviewView.as_view()),
+               path('/category', CategoryView.as_view()),
+               path('/<int:product_id>/review', ReviewView.as_view()),
 ]
