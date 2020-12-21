85f5379581716caa5c8a21c7f46a71103f37352f
Hailee <haileeyu21@gmail.com>
2020-12-21 20:10:55 +0900

    [유하람] '오늘'탭 게시물 목록 조회 - 최신댓글 표출 ver. (메서드화)

--- board/views.py
+++ board/views.py
@@ -9,60 +9,42 @@ from user.models    import Member, BoardLike, CommentLike
 # 1. 동묘프렌즈 첫 접속 - '오늘' 탭 게시물 로드
 class BoardListView(View):
     def get(self, request):
-        try:
-            board_data  = Board.objects.all()
-            boards       = []
-            # 전체 게시물 개수만큼 반복
-            for i in board_data:
-                board_id    = i.id
-                # 가장 최근의 댓글 목록[0] 가져오기
-                comments      = []
-                ## 1. 해당 게시물의 댓글 id 추출
-                comment_list = Comment.objects.filter(
-                                    board_id = board_id
-                                )
-                comment_ids = []
-                for j in range(len(comment_list)):
-                    comment_ids.append(comment_list[j].id)
-                ## 2. 각 댓글의 좋아요 수 sum list 중 가장 큰 값 추출 
-                comment_sums = {} 
-                for j in range(len(comment_ids)):
-                    comment_sums[comment_ids[j]] = CommentLike.objects.filter(comment_id =comment_ids[j]).count()
-                # 3. 댓글이 아예 없는 경우 대비하기
-                if comment_ids:
-                    best_comment_id = max(comment_sums, key = comment_sums.get)
-                    comment_data = comment_list.filter(id = best_comment_id)
-                    # 댓글이 있을 경우 board 리스트에 담을 준비
-                    if comment_data:
-                        member_id = comment_data[0].writer_id
-                        comments.append({
-                            'comment_num'   : len(comment_data),
-                            'writer'        : Member.objects.filter(id = comment_data[0].writer_id)[0].nickname,
-                            'content'       : comment_data[0].content
-                        })
-                # board image 준비
-                board_images        = []
-                board_image_data    = BoardImage.objects.filter(board_id = board_id)
-                for k in range(len(board_image_data)):
-                    board_images.append(
-                        board_image_data[k].image_url
-                    )
-                # 게시물 좋아요 수 가져오기
-                board_likes = BoardLike.objects.filter(board_id = board_id)
-                
-                # '오늘' 탭 목록을 위한 board 리스트 준비
-                boards.append({
-                    'board_id'      : i.id,
-                    'uploader'      : i.uploader,
-                    'theme'         : i.theme,
-                    'title'         : i.title,
-                    'content'       : i.content,
-                    'created_at'    : i.created_at,
-                    'comment'       : comments,
-                    'board_image'   : board_images,
-                    'board_likes'   : len(board_likes)
-                })
-            return JsonResponse({'message' : 'SUCCESS', 'board_list' : boards}, status = 200)
+        try:            
+            # '오늘' 탭 목록을 위한 board 리스트 준비 (전체 게시물 개수만큼 반복)
+            ## board image 준비
+            ## 게시물 좋아요 수 가져오기
+            ## 가장 최근의 댓글 가져오기(댓글이 아예 없는 경우 대비)
+            comment_data_writer = Comment.objects.filter(board_id = 3).only('writer').order_by('-created_at').first()
+            comment_data_content = Comment.objects.filter(board_id = 3).only('content').order_by('-created_at').first()
+            #print(Comment.objects.filter(board_id = 3).order_by('-created_at').first().writer.nickname)
+            #print(Comment.objects.filter(board_id = 3).order_by('-created_at').first().content)
+            
+            def check_comment(self, board_id):
+                print(board_id)
+                if Comment.objects.filter(board_id = board_id).exists() : 
+                    data = Comment.objects.filter(board_id = board_id).order_by('-created_at').first()
+                    comment_data = {
+                        "writer"    : data.writer.nickname,
+                        "content"   : data.content
+                    }
+                else:
+                    comment_data = ''
+                return comment_data
+
+            
+            board_list = [{
+                "id"            : board.id,
+                "uploader"      : board.uploader,
+                "theme"         : board.theme,
+                "title"         : board.title,
+                "content"       : board.content,
+                "created_at"    : board.created_at,
+                "board_images"  : list(BoardImage.objects.filter(board_id = board.id).values('image_url')),
+                "board_likes"   : BoardLike.objects.filter(board_id = board.id).count(),
+                "comment"       : check_comment(self,board.id)
+            } for board in Board.objects.all()]  
+
+            return JsonResponse({'message' : 'SUCCESS', 'board_list' : board_list}, status = 200)
 
         except KeyError:
             return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from user.views  import SignUpView, LoginView
+from user.views  import SignUpView
 
 urlpatterns=[
     path('/signup', SignUpView.as_view()),
-    path('/login',LoginView.as_view())
+    #path('/login',LoginView.as_view())
 ]
 
