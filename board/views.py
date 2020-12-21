bf4a66378ce82124b51047116200b1eab583f0c6
Hailee <haileeyu21@gmail.com>
2020-12-21 20:28:05 +0900

    [유하람] '오늘'탭 게시물 목록 조회 - 최신댓글 표출 ver. (메서드화) 최종 - getBoardView 삭제

--- board/views.py
+++ board/views.py
@@ -42,21 +42,7 @@ class BoardListView(View):
         except Board.MultipleObjectsReturned:
             return JsonResponse({'message' : 'TOO_MANY_DATA'}, status = 500)
         
-# 2. 게시물 조회 - 하나의 게시물 & 댓글 & 좋아요 - select_related() 사용해볼것?
-class GetBoardView(View):
-    def get(self, request):
-        try:
-            
-            return JsonResponse({'message' : 'SUCCESS', 'board_list' : boards}, status = 200)
-        
-        except KeyError:
-            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
-        except ValueError:
-            return JsonResponse({'message' : 'DECODE_ERROR'}, status = 400)
-        except Board.DoesNotExist:
-            return JsonResponse({'message' : 'NO_EXIST_DATA'}, status = 500)
-        except Board.MultipleObjectsReturned:
-            return JsonResponse({'message' : 'TOO_MANY_DATA'}, status = 500)
+
 
 
 
