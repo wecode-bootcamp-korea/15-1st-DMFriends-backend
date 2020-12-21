3e2d623658b9175cf77c0c2c4eedd707345db558
lyla0427 <hillary0427@gmail.com>
2020-12-21 18:42:18 +0900

    Refactor: Order app 제거 후 다시 커밋

--- dmfriends/urls.py
+++ dmfriends/urls.py
@@ -17,5 +17,4 @@ from django.urls import path, include
 
 urlpatterns = [
     path('product', include('product.urls')),
-    #path('order', include('order.urls'))
 ]
--- product/views.py
+++ product/views.py
@@ -1,9 +1,10 @@
 import json
+import decimal
 
 from django.views     import View
 from django.http      import JsonResponse
  
-from .models     import  (
+from .models      import  (
     Category,
     Subcategory, 
     Product, 
@@ -11,35 +12,65 @@ from .models     import  (
     Discount, 
     Review
 )
-from user.models import Member, login_decorator
 
-class ProductDetailView(View):
-    def get(self, request, product_id):
-            if Product.objects.filter(id=product_id).exists():
-                product =  list(Product.objects.filter(id=product_id).values())
-                return JsonResponse({"result" : product_list}, status = 200)
-            return JsonResponse({"message" : "PRODUCT_DOES_NOT_EXIST"}, status=404)
-#한국어 인코딩이 안됨
+from user.models  import Member #login_decorator
 
+
+class ProductListView(View):
     def get(self, request):
             category_seq    = request.GET.get('category', None)
             subcategory_seq = request.GET.get('subcategory', None)
             sort            = request.GET.get('sort', None)
 
             if subcategory_seq == None:
-                product_list =  list(Product.objects.filter(category=category_seq).order_by(sort).values())
+                products = Product.objects.filter(category=category_seq).order_by(sort).values()
             else:
-                product_list =  list(Product.objects.filter(category=category_seq, subcategory=subcategory_seq).order_by(sort).values())
+                products = Product.objects.filter(category=category_seq, subcategory=subcategory_seq).order_by(sort).values()
+            filtered_products = [{
+                "id"            : item["id"],
+                "name"          : item["name"],
+                "price"         : item["price"],
+                "star_rating"   : item["star_rating"],
+                "description"   : item["description"],
+                "category_id"   : item["category_id"],
+                "subcategory_id": item["subcategory_id"],
+                "discount_id"   : item["discount_id"],
+                "image_url"     : item["image_url"],
+                "created_at"    : item["created_at"],
+            }for item in products]
 
-            return JsonResponse({"message" : "SUCCESS", "result" : product_list}, status = 200)
+            return JsonResponse({"message" : "SUCCESS", "result" : filtered_products}, status = 200)
+            
+class ProductDetailView(View):
+    def get(self, request, product_id):
+            if Product.objects.filter(id=product_id).exists():
+                products =  Product.objects.filter(id=product_id).values()
+
+                product_list = [{
+                    "id"            : item["id"],
+                    "name"          : item["name"],
+                    "price"         : item["price"],
+                    "star_rating"   : item["star_rating"],
+                    "description"   : item["description"],
+                    "category_id"   : item["category_id"],
+                    "subcategory_id": item["subcategory_id"],
+                    "discount_id"   : item["discount_id"],
+                    "image_url"     : item["image_url"],
+                    "created_at"    : item["created_at"],
+                }for item in products]            
+                
+                return JsonResponse({"result" : product_list}, status = 200)
+            return JsonResponse({"message" : "PRODUCT_DOES_NOT_EXIST"}, status=404)
+#한국어 인코딩이 안됨
 
 class ReviewView(View):
-    @login_decorator #데코레이터가 user앱에 있어서 머지되었다는 가정하에 코드를 짰습니다.
+    #@login_decorator
     def post(self, request, id):
-        data        = json.loads(request.body)
-        
+        data = json.loads(request.body)
+        member_ins = Member.objects.get(id=request.user.id)
+
         if Product.objects.filter(id=product_id).exists():
-            member_ins = Member.objects.get(id=request.user.id) 
+            member_ins = Member.objects.get(id=member_ins) 
             Review.objects.create(star_rating=data['star_rating'], created_at=data['created_at'] , product=product_id, member=member_ins, content = data['content']) 
 
         return JsonResponse({"message" : "SUCCESS", "result" : data}, status = 200)
