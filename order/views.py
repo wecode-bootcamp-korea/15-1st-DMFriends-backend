c7d89d4bbdd2c4de4f66ff8e1279a69e69ad88f7
lyla0427 <hillary0427@gmail.com>
2020-12-23 16:43:35 +0900

    Refactor: 멘토 코멘트 반영 - 카트에 담긴 상품 리스트를 뽑는 함수가 분리되어있었으나 불필요하다는 의견 반영하여 get함수에 흡수

--- order/views.py
+++ order/views.py
@@ -9,42 +9,32 @@ from product.models import Product, ProductImage
 import random
 #from user.utils     import login_decorator
 
-def product_list(self, product_id, message):
-
-    cart_single_product   = Cart.objects.get(product_id=product_id)
-    product_added = cart_single_product.product
-
-    result = {
-        "message"       : message,
-        "id"            : product_added.id,
-        "name"          : product_added.name,
-        "price"         : int(product_added.price),
-        "quantity"      : cart_single_product.quantity,
-        "discount_id"   : product_added.discount_id,
-        "image_url"     : str(ProductImage.objects.filter(product_id = product_added.id).values_list('image_url', flat=True)),
-                }
-    return result
-
-
 class CartView(View):
     def get(self, request):
-        cart_all_product = Cart.objects.values_list('product_id', flat=True).distinct()
-        all_products = [{item: product_list(self, item, '')} for item in cart_all_product]
+        cart = Cart.objects.all()
+        result = [{
+            "id"            : item.product.id,
+            "name"          : item.product.name,
+            "price"         : int(item.product.price),
+            "quantity"      : item.quantity,
+            "discount_id"   : item.product.discount_id,
+        }for item in cart]
 
-        return JsonResponse({"message" : "SUCCESS", "result" : all_products}, status = 200)
+        return JsonResponse({"message" : "SUCCESS", "result" : result}, status = 200)
     
     #@login_decorator
     def post(self, request):
+
+        ORDER_STATUS_ID = 1
+
         data = json.loads(request.body)
-        order_status_id = OrderStatus.objects.get(id=1)
         order_ins, created = Order.objects.get_or_create(       
-            order_status_id = order_status_id,
+            order_status_id = ORDER_STATUS_ID,
             defaults     = {
                 'order_number'     : random.randint(100, 999),
                 'address'          : Address.objects.get(address="justforcart"),
                 'member'           : Member.objects.get(id=1), #임시로 1번!
-                'delivery_message' : '', 
-                'order_date'       : 00000, 
+                'delivery_message' : '',
                 'payments'         : Payment.objects.get(kakao_pay="justforcart")
                 },
         )
@@ -61,7 +51,7 @@ class CartView(View):
         
         Cart.objects.create(quantity=data["quantity"], created_at=00000, product=product_ins, total_price=price_ins, order=order_ins)             
         
-        return JsonResponse({"message" : "SUCCESS", "result" : '장바구니에 상품이 담겼습니다'}, status = 200)
+        return JsonResponse({"message" : "SUCCESS"}, status = 200)
     
     def delete(self, request):
         product_ids = request.GET.getlist('product_id', None)
@@ -82,10 +72,8 @@ class CartModifyView(View):
         cart.total_price = cart.product.price * int(data["quantity"])
         cart.save()
 
-        modified_product_info = product_list(self, cart.product_id, "장바구니에서 상품의 갯수가 변경되었습니다")
-
-        return JsonResponse({'message' : 'SUCCESS', "result" : modified_product_info}, status = 200)
+        return JsonResponse({'message' : 'SUCCESS'}, status = 200)
 
     def delete(self, request, cart_id): #장바구니에서 직접 삭제하기 (Path parameter)
         Cart.objects.filter(id=cart_id).delete()
-        return JsonResponse({"message" : "SUCCESS", "result" : '장바구니에서 상품이 삭제되었습니다'}, status = 200)
+        return JsonResponse({"message" : "SUCCESS"}, status = 200)
